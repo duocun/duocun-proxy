9797eb33d864c480158973e8b669f4a67b05ec87
brianshen <shenhw3@asiainfo-sec.com>
2020-04-29 11:22:36 +0800

    #7 Add logs to trace why white screen happens
    https://github.com/duocun/duocun-proxy/issues/7

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from './auth.service';
-import { LogService } from '../log.service';
+import { LogService, LogEventWhiteScreenType } from '../log.service';
 import { Subject } from 'rxjs';
 
 import {takeUntil} from 'rxjs/operators';
@@ -24,6 +24,8 @@ export class AuthComponent implements OnInit, OnDestroy {
   title = 'duocun-proxy';
   hasCode = true;
   onDestroy$ = new Subject();
+  timerHandler = null;
+  authTimeTooLong = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -41,12 +43,31 @@ export class AuthComponent implements OnInit, OnDestroy {
     // } else {
     //   window.location.href = 'https://duocun.ca';
     // }
+    this.clearTimer();
     window.location.href =
       'https://duocun.ca/grocery?token=' + tokenId + '&state=123'; //  + appCode;
     // });
   }
+  setTimer(tokenInfo:string) {
+    if ( !this.timerHandler ) {
+      // if auth time is greater than 10 seconds,
+      // try to tell clients to login again
+      this.timerHandler = setTimeout( ()=> {
+        this.logSvc.saveWhiteScreenLog(`Proxy: auth more than 10 seconds ${tokenInfo}`, LogEventWhiteScreenType.Exception);
+        this.authTimeTooLong = true;
+        this.clearTimer();
+      }, 10000)
+    }
+  }
+  clearTimer() {
+    if ( this.timerHandler ) {
+      clearTimeout(this.timerHandler);
+      this.timerHandler = null;
+    }
+  }
 
   ngOnDestroy() {
+    this.clearTimer();
     this.onDestroy$.next();
     this.onDestroy$.complete();
   }
@@ -55,6 +76,8 @@ export class AuthComponent implements OnInit, OnDestroy {
   // dev:        http://localhost:5005/?code=071uZnPi1xT97t0OFnTi12xDPi1uZnPK&state=123
   // production: http://duocun.com.cn/?code=071uZnPi1xT97t0OFnTi12xDPi1uZnPK&state=123
   ngOnInit() {
+    // set a timer, if auth takes too long, then tell users to login again
+    let whiteScreenLog = "Proxy OnInit";
     // console.log('ngOnInit');
     // log.debug(`ngOnInit called ....`);
     this.route.queryParamMap
@@ -62,33 +85,48 @@ export class AuthComponent implements OnInit, OnDestroy {
         takeUntil(this.onDestroy$)
       )
       .subscribe((queryParams) => {
+        whiteScreenLog += "->Map Done";
         const code = queryParams.get('code');
         const appCode = queryParams.get('state'); // no use at all
-
+        this.setTimer(`(${code},${appCode})`); 
+        whiteScreenLog+= `(${code},${appCode})`;
         // process wx 40163 issue
         const { accessToken, openId } = this.authSvc.getWechatOpenId();
         if (accessToken && openId) {
+          whiteScreenLog+= "->Have local accessToken"
           this.authSvc
             .wechatLoginByOpenId(accessToken, openId)
             .pipe(takeUntil(this.onDestroy$))
             .subscribe((r: any) => {
               if (r && r.tokenId) {
                 this.authSvc.setAccessTokenId(r.tokenId);
+                whiteScreenLog += `->WeChat login ok, redirect`;
+                this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Success);
                 this.redirectApp(appCode, r.tokenId);
               } else {
+                whiteScreenLog += `->WeChat login fail ${r}`;
+                this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Failure);
+                this.clearTimer();
                 // accessToken expiry
                 alert('微信登陆失败，请退出公众号重新尝试');
                 return;
               }
+            }, error => {
+              whiteScreenLog+= `->wechatLoginByOpenId subscribe error : ${error}`;
+              this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Exception);
             });
         } else {
+          whiteScreenLog+= "->No local accessToken"
           // if accessToken expired
-          this.wechatLoginByCode(appCode, code);
+          this.wechatLoginByCode(appCode, code, whiteScreenLog);
         }
+      }, error => {
+        whiteScreenLog+= `->Map subscribe error : ${error}`;
+        this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Exception);
       });
   }
 
-  wechatLoginByCode(appCode, code) {
+  wechatLoginByCode(appCode, code, whiteScreenLog) {
     if (appCode && code) {
       this.authSvc
         .wechatLoginByCode(code)
@@ -99,11 +137,23 @@ export class AuthComponent implements OnInit, OnDestroy {
           if (r && r.tokenId) {
             this.authSvc.setWechatOpenId(r.accessToken, r.openId, r.expiresIn);
             this.authSvc.setAccessTokenId(r.tokenId); // duocun jwt token
+            whiteScreenLog += `->WeChat login ok, redirect`;
+            this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Success);
             this.redirectApp(appCode, r.tokenId); // duocun jwt token
           } else {
+            whiteScreenLog += `->WeChat login fail ${r}`;
+            this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Failure);
+            this.clearTimer();
             alert('微信登陆失败, 请退出重新尝试');
           }
+        }, error => {
+          whiteScreenLog += `->wechatLoginByCode subscribe err ${error}`;
+          this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Exception);
         });
+    } else {
+      // we log this as exception 
+      whiteScreenLog += "->Param error";
+      this.logSvc.saveWhiteScreenLog(whiteScreenLog, LogEventWhiteScreenType.Exception);
     }
   }
 }
--- src/app/log.service.ts
+++ src/app/log.service.ts
@@ -4,11 +4,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { AuthService } from './auth/auth.service';
 
+export enum LogEventWhiteScreenType {
+  Success = "WhiteScreen Success",
+  Failure = "WhiteScreen Failure",
+  Exception = "WhiteScreen Exception",
+} 
+
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
   url;
+  // only send success login once
+  whiteScreenLogState = {
+    [LogEventWhiteScreenType.Success] : 0,
+    [LogEventWhiteScreenType.Failure] : 0,
+    [LogEventWhiteScreenType.Exception] : 0
+  };
   constructor(
     private http: HttpClient,
     private authSvc: AuthService
@@ -34,5 +46,25 @@ export class LogService {
 
   save(data) {
     return this.doPost(this.url, data).toPromise();
+  }
+  saveWhiteScreenLog( message: string, whiteScreenEventType: LogEventWhiteScreenType ){
+    const data = {
+      message: message,
+      type: whiteScreenEventType.toString()
+    };
+    this.whiteScreenLogState[ whiteScreenEventType ]++;
+
+    if ( whiteScreenEventType === LogEventWhiteScreenType.Exception
+      ) {
+        // Don't await to hang user's login action, just try to send
+        // currently send all types, gradually only show exception 
+        this.save(data);
+      } else {
+        if ( this.whiteScreenLogState[ whiteScreenEventType ] <= 1 ) { 
+          // only send once, otherwise, when switch tabs, it will send
+          this.save(data);
+          // console.log(data); 
+        }
+      }
   }
 }
